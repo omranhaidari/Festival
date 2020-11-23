db139c008fd565a9ad7e8cc2cc61b679dcfd5062
omranhaidari <omran.haidari@etu.univ-lyon1.fr>
2020-11-23 23:08:32 +0100

    Ajout de la notification et de la vibration

---
+++ app/src/main/java/com/example/festival/MessagingServiceFirebase.java
@@ -0,0 +1,65 @@
+package com.example.festival;
+
+import android.app.IntentService;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MessagingServiceFirebase extends com.google.firebase.messaging.FirebaseMessagingService {
+
+    private static final String CANAL = "notificationCanal";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        String bodyMessage = remoteMessage.getNotification().getBody();
+        String titleMessage = remoteMessage.getNotification().getTitle();
+
+        Log.d("FirebaseMessage", "Vous venez de recevoir une notification : " + bodyMessage);
+
+        // Création d'une notification
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, CANAL);
+        notificationBuilder.setContentTitle(titleMessage);
+        notificationBuilder.setContentText(bodyMessage);
+
+        // Ajout de l'icône alarme dans la notification
+        notificationBuilder.setSmallIcon(R.drawable.alarm);
+
+        // Envoi de la notification
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        // Pour la version Oreo
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String channelId = getString(R.string.notification_channel_id);
+            String channelTitle = getString(R.string.notification_channel_title);
+            String channelDescription = getString(R.string.notification_channel_description);
+            NotificationChannel channel = new NotificationChannel(channelId, channelTitle, NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription(channelDescription);
+            notificationManager.createNotificationChannel(channel);
+            notificationBuilder.setChannelId(channelId);
+        }
+
+        // Redirection vers l'application (écran de chargement)
+        Intent intent = new Intent(getApplicationContext(), SplashScreen.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,  intent, 0);
+
+        // Ajout de la redirection
+        notificationBuilder.setContentIntent(pendingIntent);
+
+        // Création de la vibration
+        long [] vibrationPattern = {500, 1000};
+        notificationBuilder.setVibrate(vibrationPattern);
+
+        notificationManager.notify(1, notificationBuilder.build());
+    }
+}
--- app/src/main/java/com/example/festival/SplashScreen.java
+++ app/src/main/java/com/example/festival/SplashScreen.java
@@ -6,20 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-public class Welcome extends AppCompatActivity {
+public class SplashScreen extends AppCompatActivity {
 
-    public static int TIME_OUT = 2000;
+    public static int TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        setContentView(R.layout.activity_splashscreen);
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(Welcome.this, MainActivity.class);
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
